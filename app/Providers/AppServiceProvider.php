6f8a5a38adbebaeb7654f14af675ab6513090f35
krls2020 <mail@karelhemza.cz>
2024-10-22 11:35:51 +0200

    v1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use A17\Twill\Facades\TwillNavigation;
+use A17\Twill\Facades\TwillAppSettings;
+use A17\Twill\Services\Settings\SettingsGroup;
+use A17\Twill\View\Components\Navigation\NavigationLink;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        TwillNavigation::addLink(
+            NavigationLink::make()->forModule('pages')
+        );
+        TwillNavigation::addLink(
+            NavigationLink::make()->forModule('menuLinks')->title('Menu')
+        );
+        TwillAppSettings::registerSettingsGroup(
+            SettingsGroup::make()->name('homepage')->label('Homepage')
+        );
     }
 }
--- routes/twill.php
+++ routes/twill.php
@@ -4,3 +4,6 @@ use A17\Twill\Facades\TwillRoutes;
 
 // Register Twill routes here eg.
 // TwillRoutes::module('posts');
+
+TwillRoutes::module('pages');
+TwillRoutes::module('menuLinks');
